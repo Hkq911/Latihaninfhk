ceef17221bb1733b25a068e15a6a74fe1ae93f35
Hkq911 <husainkeren210@gmail.com>
2025-05-21 09:11:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Selamat Datang Di Web Informatika ")
 st.write(
-    "Ngoding seru bersama Bapak Hendri Seriadi"
+    "Ngoding seru bersama Bapak Hendri Setiadi"
 )
 st.image("20250426_001012.jpg", width= 200)
